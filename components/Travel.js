85404f8b84275668e0ea163087f96ce4cba75e29
Paul Brighton <brighton.paul@yahoo.com>
2019-03-30 11:47:44 +0000

    Home page layout is almost completed (#4)
    * Home page mobile layout done but needs tweeking and media queries adding
    
    * Changed recipe category items to cards and made other minor tweeks to home page

--- components/Travel.js
+++ components/Travel.js
@@ -3,29 +3,21 @@ import Link from 'next/link'
 
 class Travel extends Component {
   render () {
-    return <div className='c-travel'>
-      {/* <section className='c-travel__container'>
-        <div className='c-travel__image'>
-          <figure className='c-travel__figure'>
-            <Link href='/'><a><img className='c-travel__image--link' src='/static/images/Japan_Korea 617.jpg' alt='Image' /></a></Link>
-            <div class='c-image__slider--text'><h4>Centered</h4></div>
-            <figcaption className='c-travel__figure-text'>Fushimi Inari Shrine</figcaption>
-          </figure>
-        </div> */}
-      
-      <div className='c-travel__image'>
-        <figure>
+    return <section className='c-travel'>
+      <div className='c-travel__container'>
+        <h2>Travel Inspiration</h2>
+        <div className='section__divider' />
+        <figure className='c-travel__image'>
           <img src='/static/images/Japan_Korea 617.jpg' />
+          <figcaption className='c-travel__image-text'><h4>Fushimi Inari Shrine</h4></figcaption>
         </figure>
-        <div className='c-image__slider--text'><h4>Fushimi Inari Shrine</h4></div>
       </div>
-      <div className='c-about__text'>
+      <div className='c-travel__text'>
         <p>YummyPixels was born when Food Photographer Paul Brighton and his wife and food stylist, Thais, realised that their photo shoot sessions conveyed their love for the food they tasted during their travels around the world.</p>
         <p>This website is result of testing recipes, cooking, styling and photographing the food that they discovered and learned to enjoy daily.</p>
         <p>The world’s diversity can easily be verified by the way each ethnic group cooks, as this is a long lasting identifier of their culture.</p>
       </div>
-      {/* </section> */}
-    </div>
+    </section>
   }
 }
 
