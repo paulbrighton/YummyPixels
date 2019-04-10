a70f31dc165ef8d95aa37677bb477bcf4648bd20
Paul Brighton <brighton.paul@yahoo.com>
2019-04-10 16:54:35 +0100

    Server side rendering updated

---
+++ env-config.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+module.exports = {
+  "env": {
+    "development": {
+      "presets": ["next/babel"],
+      "plugins": [["transform-define", env]]
+    },
+    "production": {
+      "presets": ["next/babel"],
+      "plugins": [["transform-define", env]]
+    },
+    "test": {
+      "presets": [["next/babel", { "preset-env": { "modules": "commonjs" } }]],
+      "plugins": [["transform-define", env]]
+    }
+  }
+}
