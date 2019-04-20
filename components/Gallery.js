e86497d0a43f0ce52945c583be8b0b3d69fc5659
Paul Brighton <brighton.paul@yahoo.com>
2019-04-20 15:09:39 +0100

    Stock photo page laid out but needs images adding

---
+++ components/Gallery.js
@@ -0,0 +1,169 @@
+import { Component } from 'react'
+
+class Gallery extends Component {
+  render () {
+    return <section className='c-gallery'>
+      <div className='c-gallery__container'>
+        <div className='c-gallery__container--row'>
+          <div className='c-gallery__container--column'>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/bo-kho.jpg' alt='Vietnamese beef stew' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/lemon-bbq-chicken.jpg' alt='Bbq lemon chicken skewers' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/borobudur.jpg' alt='Buddha at Borobudur temple' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/british-lilac-cat-2.jpg' alt='British lilac cat' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/colorful-dumplings.jpg' alt='Yellow Chinese dumplings' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/maneki-neko.jpg' alt='maneki neko' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+          </div>
+
+          <div className='c-gallery__container--column'>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/lemon-bbq-chicken.jpg' alt='Bbq lemon chicken skewers' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/borobudur.jpg' alt='Buddha at Borobudur temple' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/maneki-neko.jpg' alt='maneki neko' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/colorful-dumplings.jpg' alt='Yellow Chinese dumplings' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/bo-kho.jpg' alt='Vietnamese beef stew' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/british-lilac-cat-2.jpg' alt='British lilac cat' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+          </div>
+
+          <div className='c-gallery__container--column'>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/british-lilac-cat-2.jpg' alt='British lilac cat' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/lemon-bbq-chicken.jpg' alt='Bbq lemon chicken skewers' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/borobudur.jpg' alt='Buddha at Borobudur temple' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/maneki-neko.jpg' alt='maneki neko' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/bo-kho.jpg' alt='Vietnamese beef stew' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/colorful-dumplings.jpg' alt='Yellow Chinese dumplings' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+          </div>
+
+          <div className='c-gallery__container--column'>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/bo-kho.jpg' alt='Vietnamese beef stew' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/lemon-bbq-chicken.jpg' alt='Bbq lemon chicken skewers' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/maneki-neko.jpg' alt='maneki neko' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/british-lilac-cat-2.jpg' alt='British lilac cat' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/colorful-dumplings.jpg' alt='Yellow Chinese dumplings' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+            <figure className='c-gallery-card'>
+              <img src='/static/Images/gallery/borobudur.jpg' alt='Buddha at Borobudur temple' className='c-gallery-card__image' />
+              <div className='c-gallery-card__overlay'>
+                <div className='c-gallery-card__text'><p>Buy Image</p></div>
+              </div>
+            </figure>
+          </div>
+        </div>
+      </div>
+    </section>
+  }
+}
+
+export default Gallery
