7ba3ca6df486b20ab35bae4d57bf8c15597b2fc4
Paul Brighton <brighton.paul@yahoo.com>
2019-05-10 14:33:49 +0100

    Added link to cookbook image

--- components/Cookbook.js
+++ components/Cookbook.js
@@ -8,13 +8,15 @@ class Cookbook extends Component {
         <h2>YummyEats Cookbook</h2>
         <div className='section__divider' />
         <div className='c-cookbook__image'>
-          <figure>
-            <img src='/static/Images/yummyeats.jpg' alt='Yummy Eats cookbook cover' />
-          </figure>
+          <a href='https://www.amazon.co.uk/dp/B01DFPXFEY' target='blank'>
+            <figure>
+              <img src='/static/Images/yummyeats.jpg' alt='Yummy Eats cookbook cover' />
+            </figure>
+          </a>
         </div>
         <div className='c-cookbook__text'>
           <h3>Get the book!</h3>
-          <p>Want the recipes availbale offline? The YummyEats cookbook is avaiable to buy as a digital download from <a href='https://www.amazon.co.uk/dp/B01DFPXFEY' className='c-portfolio-project__link'>Amazon</a> or <a href='https://itunes.apple.com/gb/book/yummy-eats/id1095148851?mt=11' className='c-portfolio-project__link'>iBooks</a>. Get your copy now!</p>
+          <p>Want the recipes availbale offline? The YummyEats cookbook is avaiable to buy as a digital download from <a href='https://www.amazon.co.uk/dp/B01DFPXFEY' target='blank' className='c-portfolio-project__link'>Amazon</a> or <a href='https://itunes.apple.com/gb/book/yummy-eats/id1095148851?mt=11' target='blank' className='c-portfolio-project__link'>iBooks</a>. Get your copy now!</p>
         </div>
       </div>
     </section>
